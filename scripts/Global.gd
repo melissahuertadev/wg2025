ae2605851653ae6a897af924aaae9ae854b924db
Melissa <melissahuerta.dev@gmail.com>
2025-08-09 17:30:06 -0500

    pantalla intereses wip

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -1,5 +1,5 @@
 extends Node
 
 # Aqui se almacenan los intereses del jugador
-enum INTERESES {BASQUET, VOLEY}
+enum INTERESES {DEPORTES, VIDEOJUEGOS, LIBROS, COCINA, CINE, ANIME}
 var main_character_intereses = []
--- scripts/Registro.gd
+++ scripts/Registro.gd
@@ -1,5 +1,5 @@
 extends Node2D
 
 
-func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/Pantalla_Intereses.tscn")
+func _on_continuar_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Pantalla2_Intereses.tscn")
