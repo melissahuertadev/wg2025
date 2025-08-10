1907d38508f9d850e7527afa914dc3542570f93a
malumunayco <malumunaycoa05@gmail.com>
2025-08-10 15:08:06 -0500

    Update PlotTwistFinal.gd
    .

--- scripts/PlotTwistFinal.gd
+++ scripts/PlotTwistFinal.gd
@@ -3,13 +3,22 @@ extends Node
 @onready var label_nombre = $lbltexto
 @onready var top_rect = $CanvasLayer/TopRect
 @onready var bottom_rect = $CanvasLayer/BottomRect
-
+@onready var saludo = $lbl_saludo
+@onready var invitacion = $lbl_invitacion
+@onready var respuesta = $lbl_respuesta
+@onready var reas = $lbl_reas
 func _ready():
 	label_nombre.text = GlobalManager.main_character_nombre
-
+	saludo.text = GlobalManager.match_messages[0]
+	invitacion.text=GlobalManager.match_messages[1]
+	respuesta.text=GlobalManager.player_messages[0]
 	await get_tree().create_timer(1).timeout
 
 	var tween = create_tween()
 	var viewport_size = get_viewport().size
 	tween.tween_property(top_rect, "position", Vector2(0, -top_rect.size.y), 0.2)
 	tween.parallel().tween_property(bottom_rect, "position", Vector2(0, viewport_size.y), 0.2)
+
+
+func _on_continuar_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Pantalla_Final.tscn")
