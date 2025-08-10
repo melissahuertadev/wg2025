422d80b284f00edf7d1c23660bedcefcb1c30333
Melissa <melissahuerta.dev@gmail.com>
2025-08-10 15:12:27 -0500

    feat: final

--- scripts/PlotTwistFinal.gd
+++ scripts/PlotTwistFinal.gd
@@ -13,3 +13,8 @@ func _ready():
 	var viewport_size = get_viewport().size
 	tween.tween_property(top_rect, "position", Vector2(0, -top_rect.size.y), 0.2)
 	tween.parallel().tween_property(bottom_rect, "position", Vector2(0, viewport_size.y), 0.2)
+
+
+func _on_continuar_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Pantalla_Final.tscn")
+	pass # Replace with function body.
