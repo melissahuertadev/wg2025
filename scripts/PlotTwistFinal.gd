612d562b5c82de0e1c54d0042e6939b14cde1b64
malumunayco <malumunaycoa05@gmail.com>
2025-08-10 09:36:00 -0500

    chat.plot

--- scripts/PlotTwistFinal.gd
+++ scripts/PlotTwistFinal.gd
@@ -1,11 +1,5 @@
 extends Node
+@onready var label_nombre = $lbltexto
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	label_nombre.text =  GlobalManager.main_character_nombre
