4de0f6d642ee02e1cf32d8e1e67cd7695dffb856
malumunayco <malumunaycoa05@gmail.com>
2025-08-09 20:56:30 -0500

    cambios-registro

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -3,3 +3,4 @@ extends Node
 # Aqui se almacenan los intereses del jugador
 enum INTERESES {DEPORTES, CINE, VIDEOJUEGOS, ANIMES, LIBROS, COCINA, FESTIVALES, MUSICA, TECH}
 var main_character_intereses = []
+var protagonista: String = " " 
