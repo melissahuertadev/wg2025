0ad527f8ac5f9fa3f3ded90abcf75c809a9a7000
malumunayco <malumunaycoa05@gmail.com>
2025-08-09 23:27:03 -0500

    cambios-registro2.0

--- scripts/Registro.gd
+++ scripts/Registro.gd
@@ -1,4 +1,7 @@
 extends Node2D
 
-func _on_continuar_button_pressed() -> void:
+func _on_btn_back_inicio_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Pantalla0_Inicio.tscn")
+
+func _on_btn_go_intereses_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Pantalla2_Intereses.tscn")
