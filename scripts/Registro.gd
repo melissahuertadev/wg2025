de4692a524eb101439061466e5961f224e26a8b6
malumunayco <malumunaycoa05@gmail.com>
2025-08-09 23:50:53 -0500

    nuevo-registro-interfaz

--- scripts/Registro.gd
+++ scripts/Registro.gd
@@ -3,5 +3,14 @@ extends Node2D
 func _on_btn_back_inicio_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Pantalla0_Inicio.tscn")
 
+
+func _on_btn_go_edad_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Pantalla1_RegistroEdad.tscn")
+
+
+func _on_btn_back_nombre_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/Pantalla1_RegistroNombre.tscn")
+
+
 func _on_btn_go_intereses_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/Pantalla2_Intereses.tscn")
